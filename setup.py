80c2d7ddf10b531708e02014faa7f1b6cb61bddb
ngmtine <ngmfries@gmail.com>
2021-08-22 17:05:07 +0900

    update: setup.py追記

--- setup.py
+++ setup.py
@@ -5,4 +5,11 @@ setup(
 	version = "0.0.1",
 	packages = find_packages(),
     author = "ngmtine",
+    url = "https://github.com/ngmtine/am-eater",
+	description = "Downloader for am-our.com",
+	long_description = open('README.md').read(),
+	long_description_content_type = 'text/markdown',
+	python_requires = ">=3.8.2",
+	install_requires = ["requests", "beautifulsoup4", "lxml"],
+	package_data = {"AmEater": ["settings.ini"]},
 )
